d5baf6126964cef6eca046de01bb704ef3d0bf3c
Muoyheangchhea <157553927+Muoyheangchhea@users.noreply.github.com>
2024-01-27 22:50:37 +0700

    update

--- evalutestring.py
+++ evalutestring.py
@@ -9,21 +9,14 @@
 # "J-J" ➞ "J"
 # Notes a hyphen will separate the two letters in the string.
 
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" #write down all of the alphabet total range
+userinput = input("Enter a range of letters (e.g., a-z): ") #ask user to input letter from one - another using split as "to" e.g. a-m
+start, end = userinput.split("-") #analyze start and end point by getting the user input range e.g. a-m so, start=a, end=m
+startnum = alphabet.index(start) #using index function to find the position of the start point e.g. a = 0
+endnum = alphabet.index(end) #using index function to find the position of the end point e.g. m = 12
+range = alphabet[startnum:endnum + 1] #using range alphabet[num1:num2] which means the position of num1 to num2 
+#e.g. alphabet[0:12] which means in alphabet variable, it displays from letter in 0 position to 12 position
+#note: variable[:] only works with number, not string and that's why we have to find the number of position in the start and end
+print("The alphabets between " + start + " and " + end + " are", range)
 
-alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-# Get user input for the letter range
-user_range = input("Enter a range of letters (e.g., a-z): ")
-
-# Extract start and end letters from the user input
-start_letter, end_letter = user_range.split('-')
-start_index = alphabet.index(start_letter)
-end_index = alphabet.index(end_letter)
-
-# Create the result string by slicing the alphabet
-result_string = alphabet[start_index:end_index + 1]
-
-# Convert the result string to uppercase if the start letter is uppercase
-
-print(result_string)
 
